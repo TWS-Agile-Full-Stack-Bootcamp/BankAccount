b547fb15a670a09e11a66c630a23829c8e1c68f1
Wu Xueheng <xhwu@thoughtworks.com>
2020-11-06 01:29:46 +0800

    feat: generate account number when open a new account

--- BankAccount/BankAccount.cs
+++ BankAccount/BankAccount.cs
@@ -3,15 +3,19 @@ namespace BankAccount
 {
     public class BankAccount
     {
-        public string Number { get; }
-        public string Owner { get; set; }
-        public decimal Balance { get; }
+        private static int accountNumberSeed = 1234567890;
 
         public BankAccount(string name, decimal initialBalance)
         {
             this.Owner = name;
             this.Balance = initialBalance;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
         }
+
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
